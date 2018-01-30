ba1fd48e53fb273f19d8de83a4ae6e5deec70fae
johnny9052 <alexander9052@gmail.com>
2018-01-30 15:45:25 -0500

    Commit inicial

---
+++ View/public/main/banner.php
@@ -0,0 +1,23 @@
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+        
+        <a id="logo-container" href="#" class="brand-logo">Logo</a>
+        
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Equipo</a></li>
+            <li><a href="#">Noticias</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="side-nav">
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Equipo</a></li>
+            <li><a href="#">Noticias</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+</nav>

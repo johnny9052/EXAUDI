983407dbce2fbb1a78255a0b50f4c20f56b0e848
johnny9052 <alexander9052@gmail.com>
2018-01-30 18:16:51 -0500

    Commit inicial

--- View/public/home.php
+++ View/public/home.php
@@ -50,7 +50,7 @@
                 <div class="icon-block">                    
                     <h5 class="center">Objetividad Profesional</h5>
 
-                    <p class="center light">Entendemos a la familia como una unidad, donde cada miembro influye en las características que la definen.</p>
+                    <p class="center light">Entendemos a la familia como una unidad, donde cada miembro influye en las características que la definen</p>
                 </div>
             </div>         
         </div>
@@ -59,39 +59,79 @@
 </div>
 
 
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-        </div>
-    </div>
-    <div class="parallax"><img src="Resources/public/image/background2.jpg" alt="Unsplashed background img 2"></div>
-</div>
+
+
 
 <div class="container">
     <div class="section">
-
+        <!--   Icon Section   -->
         <div class="row">
-            <div class="col s12 center">
-                <h3><i class="mdi-content-send brown-text"></i></h3>
-                <h4>Contact Us</h4>
-                <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-            </div>
+            <div class="col s12">
+                <div class="icon-block">                    
+                    <h5 class="center">Honestidad</h5>
+
+                    <p class="center light">Nuestro cliente es la familia y su patrimonio, les orientamos y diseñamos soluciones adaptadas a sus necesidades reales</p>
+                </div>
+            </div>         
         </div>
 
     </div>
 </div>
 
 
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
+
+
+<div class="container">
+    <div class="section">
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12">
+                <div class="icon-block">                    
+                    <h5 class="center">Confidencialidad</h5>
+
+                    <p class="center light">Somos depositarios de la confianza de nuestros clientes. Honramos el voto de confidencialidad con cuidado y espíritu profesional</p>
+                </div>
+            </div>         
+        </div>
+
+    </div>
+</div>
+
+
+
+<div class="container">
+    <div class="section">
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12">
+                <div class="icon-block">                    
+                    <h5 class="center">Empatía</h5>
+
+                    <p class="center light">Guiamos desde la experiencia y la plena compresión de las dinámicas familiares, conectando con los proyectos y expectativas del cliente</p>
+                </div>
+            </div>         
+        </div>
+
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="section">
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12">
+                <div class="icon-block">                    
+                    <h5 class="center">AQUI VA UNA IMAGEN</h5>
+
+                    <h5 class="center">Nuestros servicios</h5>
+                </div>
+            </div>         
         </div>
     </div>
-    <div class="parallax"><img src="Resources/public/image/background3.jpg" alt="Unsplashed background img 3"></div>
 </div>
+
+
+
+
+

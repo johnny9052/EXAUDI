7df94f937f4ca0085be74adfadd62e9c727846bd
DAGA <DAGA@DAGA-PC.eam.edu.co>
2018-01-30 19:28:12 -0500

    Avances en team

--- View/public/team.php
+++ View/public/team.php
@@ -1,28 +1,3 @@
-
-
-
-<!--HOME FRASE INICIAL-->
-<div id="index-banner" class="parallax-container">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <br><br>
-            <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
-            <div class="row center">
-                <h5 class="header col s12 light">Guiamos a las empresas y patrimonios familiares en la transmisión y sostenibilidad de su legado
-                </h5>
-            </div>
-            <div class="row center">
-                <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
-            </div>
-            <br><br>
-
-        </div>
-    </div>
-    <div class="parallax"><img src="Resources/public/image/background1.jpg" alt="Unsplashed background img 1"></div>
-</div>
-<!--END HOME FRASE INICIAL-->
-
-
 <div class="container">
     <div class="section">
 
@@ -39,7 +14,7 @@
 
             </div>
         </div>
-        
+
         <div class="row">
             <div class="col s12 m1">
 
@@ -53,7 +28,7 @@
 
             </div>
         </div>
-        
+
         <div class="row">
             <div class="col s12 m12">
                 <div class="icon-block">
@@ -61,7 +36,7 @@
                 </div>
             </div>
         </div>
-        
+
 
         <!--   Icon Section   -->
         <div class="row">
@@ -117,7 +92,7 @@
 
             </div>
         </div>
-        
+
         <div class="row">
             <div class="col s12 m1">
 
@@ -125,14 +100,14 @@
             <div class="col s12 m10">
                 <div class="left">
                     <p class="consultantDesc">Como consultores nos apoyamos en profesionales de alto perfil como complemento perfecto en la generación de valor para nuestros
-                                                clientes y relacionados.</p>
+                        clientes y relacionados.</p>
                 </div>
             </div>
             <div class="col s12 m1">
 
             </div>
         </div>
-        
+
         <div class="row">
             <div class="col s12 m1">
 
@@ -147,117 +122,84 @@
                     <h4 class="center">STAFF</h4>
                 </div>
             </div>
-            
-            <div class="col s12 m1">
-
-            </div>
-        </div>
-        
-          <div class="row">
-            <div class="col s12 m1">
-
-            </div>
-            <div class="col s12 m2">
-                <div class="icon-block">
-                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                    <h6 class="center">GUILLERMO SALAZAR</h6>
-
-                    <p class="consultantDesc">CEO - Consultant</p>
-                </div>
-            </div>
-
-            <div class="col s12 m2">
-                <div class="icon-block">
-                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                    <h6 class="center">GUILLERMO SALAZAR</h6>
-
-                    <p class="consultantDesc">CEO - Consultant</p>
-                </div>
-            </div>
-
-            <div class="col s12 m2">
-                <div class="icon-block">
-                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                    <h6 class="center">GUILLERMO SALAZAR</h6>
-
-                    <p class="consultantDesc">CEO - Consultant</p>
-                </div>
-            </div>
-
-            <div class="col s12 m2">
-                <div class="icon-block">
-                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                    <h6 class="center">GUILLERMO SALAZAR</h6>
-
-                    <p class="consultantDesc">CEO - Consultant</p>
-                </div>
-            </div>
-
-            <div class="col s12 m2">
-                <div class="icon-block">
-                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                    <h6 class="center">GUILLERMO SALAZAR</h6>
-
-                    <p class="consultantDesc">CEO - Consultant</p>
-                </div>
-            </div>
 
             <div class="col s12 m1">
 
             </div>
         </div>
-        
-        <div class="row">
-            <div class="col s12 m10">
-                <div class="left">
-                    <p class="consultantDesc">NUESTROS SERVICIOS ESTÁN DISEÑADOS PARA ATENDER CADA UNA DE LAS UNIDADES DENTRO DE LA COMPLEJIDAD QUE REPRESENTA LA FAMILIA EMPRESARIAL.
-                        <br> 
-                        ACOMPAÑADOS DE UNA VISIÓN HOLÍSTICA QUE LOS INTEGRA Y UNA METODOLOGÍA EFECTIVA
-                        <br>
-                    </p>
-                    <button> NUESTROS SERVICIOS</button>
-                </div>
-                
-            </div>
-        </div>
-
-    </div>
-</div>
-
-
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-        </div>
-    </div>
-    <div class="parallax"><img src="Resources/public/image/background2.jpg" alt="Unsplashed background img 2"></div>
-</div>
-
-<div class="container">
-    <div class="section">
 
         <div class="row">
-            <div class="col s12 center">
-                <h3><i class="mdi-content-send brown-text"></i></h3>
-                <h4>Contact Us</h4>
-                <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+            <div class="col s12 m1">
+
+            </div>
+            <div class="col s12 m2">
+                <div class="icon-block">
+                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <h6 class="center">GUILLERMO SALAZAR</h6>
+
+                    <p class="consultantDesc">CEO - Consultant</p>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="icon-block">
+                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <h6 class="center">GUILLERMO SALAZAR</h6>
+
+                    <p class="consultantDesc">CEO - Consultant</p>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="icon-block">
+                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <h6 class="center">GUILLERMO SALAZAR</h6>
+
+                    <p class="consultantDesc">CEO - Consultant</p>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="icon-block">
+                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <h6 class="center">GUILLERMO SALAZAR</h6>
+
+                    <p class="consultantDesc">CEO - Consultant</p>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="icon-block">
+                    <img src="Resources/public/image/team/consultor1.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <h6 class="center">GUILLERMO SALAZAR</h6>
+
+                    <p class="consultantDesc">CEO - Consultant</p>
+                </div>
+            </div>
+
+            <div class="col s12 m1">
+
             </div>
         </div>
 
     </div>
 </div>
 
+<div class="row verdeExaudi">
+    <div class="col s12 m1">
 
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-        </div>
     </div>
-    <div class="parallax"><img src="Resources/public/image/background3.jpg" alt="Unsplashed background img 3"></div>
+    <div class="col s12 m6">
+        <div class="left">
+            <p class="verdeExaudi">NUESTROS SERVICIOS ESTÁN DISEÑADOS PARA ATENDER CADA UNA DE LAS DIMENSIONES DENTRO DE LA COMPLEJIDAD QUE REPRESENTA LA FAMILIA EMPRESARIAL.
+                <br> 
+                ACOMPAÑADOS DE UNA VISIÓN HOLÍSTICA QUE LOS INTEGRA Y UNA METODOLOGÍA EFECTIVA
+                <br>
+            </p>
+
+        </div>
+
+    </div>
+    <div class=" btn-large verdeExaudi botonExaudi">Get Started
+    </div>
 </div>
